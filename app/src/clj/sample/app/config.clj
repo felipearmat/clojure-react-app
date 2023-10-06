5a1635794ec23501d4482b0f2b1783dd312b02a7
Felipe A Matos <felipearmat@gmail.com>
2023-10-06 16:53:39 -0300

    Auto-loading queries from folder

--- app/src/clj/sample/app/config.clj
+++ app/src/clj/sample/app/config.clj
@@ -4,6 +4,34 @@
 
 (def ^:const system-filename "system.edn")
 
+(defn- list-files
+  "Returns a seq of java.io.Files inside a path and all of it's subfolders"
+  [path]
+  (filter #(.isFile %) (file-seq (clojure.java.io/file path))))
+
+(defn- filenames-by-folder
+  "Returns a vector of filename paths relative to 'resources'
+   folder to be used on system-file configuration"
+  [folder]
+  (let [fullpath  (str "./resources/" folder)
+        all-files (list-files fullpath)
+        filenames (filter #(re-find #".sql" (.getPath %)) all-files)]
+    (mapv #(clojure.string/replace (str %) #"./resources/" "") filenames)))
+
+(defn handle-folder-key-config
+  "If system-config has a [:db.sql/query-fn :folder] key, change it
+   to a [:db.sql/query-fn :filenames] key and updates its values for
+   a vector of sql files found on './resources/<original-path>'"
+  [config]
+  (let [query-config     (:db.sql/query-fn config)
+        folder-config    (:folder query-config)]
+    (if (seq folder-config)
+      (->> (filenames-by-folder folder-config)
+           (assoc query-config :filenames)
+           (assoc config :db.sql/query-fn))
+      config)))
+
 (defn system-config
   [options]
-  (config/read-config system-filename options))
+  (handle-folder-key-config
+    (config/read-config system-filename options)))
