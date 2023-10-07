adefb0cdac81cebf32c4d463295969f613445cb5
Felipe A Matos <felipearmat@gmail.com>
2023-10-07 19:44:06 -0300

    Updating frontend and testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+/** @jsxImportSource @emotion/react */
+import logo from "./assets/logo.svg";
+import { Global, css, keyframes } from "@emotion/react";
+
+const GlobalCss = (
+  <Global
+    styles={css`
+      body {
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+          'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+          sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+      }
+      code {
+        font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
+      }}
+      `}
+  />
+);
+
+const Rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Styles = {
+  app: css`
+    text-align: center;
+  `,
+  logo: css`
+    height: 40vmin;
+    pointer-events: none;
+    @media (prefers-reduced-motion: no-preference) {
+      animation: ${Rotate} infinite 20s linear;
+    }
+  `,
+  header: css`
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+  `,
+  link: css`
+    color: #61dafb;
+  `,
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div css={Styles.app}>
+      {GlobalCss}
+      <header css={Styles.header}>
+        <img src={`${logo}`} css={Styles.logo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          css={Styles.link}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
