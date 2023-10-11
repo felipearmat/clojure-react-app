bae98d8dfb218f11ba5c3a8dddbe72d28d5602cb
Felipe A Matos <felipearmat@gmail.com>
2023-10-11 10:05:36 -0300

    Adding specs to User model

--- app/src/clj/sample/app/web/models/users.clj
+++ app/src/clj/sample/app/web/models/users.clj
@@ -1,24 +1,40 @@
 (ns sample.app.web.models.users
   (:require
     [buddy.hashers :as hashers]
+    [clojure.string :as string]
     [clojure.spec.alpha :as spec]
-    [sample.app.web.models.utils :refer [query-fn]]))
+    [sample.app.web.models.utils :refer [query-fn validate-spec db-error]]))
 
-(def trusted-algs #{:argon2id :bcrypt+blake2b-512})
+(def trusted-algs #{:bcrypt+blake2b-512})
+
+(def password-regex
+  #"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}")
+
+(def email-regex #"^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$")
+
+(spec/def ::email (spec/and string? #(re-matches email-regex %)))
+
+(spec/def ::password (spec/and string? #(re-matches password-regex %)))
+
+(spec/def ::create-user (spec/keys :req-un [::email ::password]))
 
 (defn encrypt
   [password]
-  (hashers/derive password {:alg :bcrypt+blake2b-512 :iterations 15}))
-
-(defn create-user!
-  [email password]
-  (let [encrypted (encrypt password)]
-    (query-fn :create-user! {:email email
-                             :password encrypted})))
+  (hashers/derive password {:alg :bcrypt+blake2b-512 :iterations 13}))
 
 (defn get-users
   [where]
   (query-fn :get-users {:where where}))
+
+(defn create-user!
+  [email password]
+  (let [data {:email (string/lower-case email) :password password}]
+    (if (empty? (get-users {:email email}))
+      (->> data
+        (validate-spec ::create-user)
+        (#(merge % {:password (encrypt password)}))
+        (query-fn :create-user!))
+      (throw (db-error "User already exists.")))))
 
 (defn get-deleted-users
   [where]
@@ -42,9 +58,14 @@
   (update-user! where {:password (encrypt new-password)}))
 
 (defn verify-password
-  [attempt where]
+  ([attempt where {:keys [update]}]
   (let [encrypted (:password (first (get-users where)))
         result (hashers/verify attempt encrypted {:limit trusted-algs})]
-    (when (:valid result)
-      (when (:update result)
-        (update-password! attempt where)))))
+    ;; TODO: Verify why (:update result) always returning 'true'
+    ;; (when (and update (:valid result))
+    ;;   (when (:update result)
+    ;;     (println "Password updated!")
+    ;;     (update-password! attempt where)))
+    (:valid result)))
+  ([attempt where]
+  (verify-password attempt where {:update false})))
--- app/src/clj/sample/app/web/models/utils.clj
+++ app/src/clj/sample/app/web/models/utils.clj
@@ -1,26 +1,37 @@
 (ns sample.app.web.models.utils
   (:require
+    [clojure.spec.alpha :as spec]
     [clojure.string :as string]
     [hugsql.parameters :refer [identifier-param-quote]]
     [integrant.repl.state :as state]))
 
+(defn spec-error
+  [spec input]
+  (ex-info (spec/explain-str spec input)
+           {:type :system.exception/business}))
+
 (defn db-error
-  [message]
-  (ex-info message
-     {:type :system.exception/business
-      :causes #{:fridge-door-open  :dangerously-high-temperature}
-      :current-temperature {:value 25 :unit :celsius}}))
+  [error]
+  (ex-info (or (ex-message error) error)
+           (merge (ex-data error)
+                  {:type :system.exception/business})))
+
+(defn validate-spec
+  [spec input]
+  (if (spec/valid? spec input)
+    input
+    (throw (spec-error spec input))))
 
 (defn query-fn [& vars]
   (try
     (apply (:db.sql/query-fn state/system) vars)
-    (catch Exception e (throw (db-error (-> e ex-cause ex-message))))))
+    (catch Exception e (throw (db-error e)))))
 
 (defn transpile-query
   [params command separator options]
     (clojure.string/join " AND "
-      (for [[field value] (get params :where)]
-        (let [expression (or (:raw value) (str " = :v" :where "."))]
+      (for [[field value] (get params command)]
+        (let [expression (or (:raw value) (str " = :v" command "."))]
           (str (identifier-param-quote (name field) options)
             expression (name field))))))
 
