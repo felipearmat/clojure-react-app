acb9abe2ce26d8bb838b63c41a528058e051fb8d
Felipe A Matos <felipearmat@gmail.com>
2023-10-13 08:20:26 -0300

    Adding some tests and creating database-rollback fixture

--- app/test/clj/sample/app/test_utils.clj
+++ app/test/clj/sample/app/test_utils.clj
@@ -1,21 +1,19 @@
 (ns sample.app.test-utils
   (:require
-    [clojure.test :refer :all]
-    [integrant.repl.state :as state]
     [integrant.repl :refer [prep]]
-    [sample.app.core :as core]))
+    [integrant.repl.state :as state]
+    [next.jdbc :as jdbc]
+    [sample.app.web.models.utils :as utils]))
 
-(defn system-state
-  []
-  (or @core/system state/system))
-
-(defn system-fixture
-  "Reset database state between each deftest"
+(defn database-rollback
+  "Wrap test on a database transaction and rollback it after testing"
   [f]
-    ;; reset database before each test to avoid data left from last deftest
-    (user/reset-db)
-    (f))
+  (let [query-fn    (:db.sql/query-fn state/system)
+        data-source (:db.sql/connection state/system)]
+    (jdbc/with-transaction [conn data-source {:isolation :serializable}]
+      (with-redefs [utils/db-connector #(partial query-fn conn)]
+        (f)
+        (.rollback conn)))))
 
-(do (user/test-prep!) (prep) (user/migrate)) ; initiate test database
-(use-fixtures :each system-fixture) ; apply system-fixture
+(do (user/test-prep!) (prep) (user/reset-db)) ; initiate test database
 (user/use-system :db.sql/query-fn) ; stablish connection with database
---
+++ app/test/clj/sample/app/web/models/users_test.clj
@@ -0,0 +1,77 @@
+(ns sample.app.web.models.users-test
+  (:require
+    [clojure.test :refer :all]
+    [clojure.string :as str]
+    [sample.app.test-utils :as test-utils]
+    [sample.app.web.models.users :refer :all]))
+
+(def valid-email "test@mail.com")
+
+(def scrambled-email "TeSt@MaIl.CoM")
+
+(def valid-password "Password@1")
+
+(use-fixtures :each test-utils/database-rollback)
+
+(deftest normalize-where-test
+  (testing "Do not add an :email uneccesarily"
+    (let [where {:anykey "anything"}
+          result (normalize-where where)]
+      (is (= (keys where) (keys result)))))
+
+  (testing "Normalize :email"
+    (let [where {:email "UsErExAmPlE@example.com"}
+          result (normalize-where where)
+          normalized (normalize-email (:email where))]
+      (is (= normalized (:email result))))))
+
+(deftest get-users-test
+  (testing "Allow keyword :all"
+    (is (= 0 (count (get-users :all)))))
+
+  (testing "Query with normalized e-mail"
+    (create-user! valid-email valid-password)
+    (let [result (first (get-users {:email scrambled-email}))]
+      (is (= valid-email (:email result))))))
+
+(deftest create-user!-test
+  (create-user! valid-email valid-password)
+  (testing "Should create a single user"
+    (is (= 1 (count (get-users {:email valid-email})))))
+
+  (testing "Email must match"
+    (is (= valid-email (:email (first (get-users :all))))))
+
+  (testing "Shouldn't allow users with same email"
+    (is (thrown? Exception
+          (create-user! valid-email valid-password)))))
+
+(deftest update-password!-test
+  (let [new-password "Password@2"
+        update-password #(update-password! % valid-email)]
+    (create-user! valid-email valid-password)
+
+    (testing "Should update with valid password"
+      (is (= 1 (update-password new-password))))
+
+    (testing "Shouldn't update with invalid password"
+      (testing "when missing special character"
+        (let [bad-password (str/replace valid-password #"@" "")]
+          (is (thrown? Exception (update-password bad-password)))))
+
+      (testing "when missing uppercase letter"
+        (let [bad-password (str/replace valid-password #"[A-Z]" "")]
+          (is (thrown? Exception (update-password bad-password)))))
+
+      (testing "when missing digit"
+        (let [bad-password (str/replace valid-password #"\d" "")]
+          (is (thrown? Exception (update-password bad-password))))))))
+
+(deftest verify-password-test
+  (create-user! valid-email valid-password)
+
+  (testing "Return true with right password"
+    (is (= true (verify-password valid-password valid-email))))
+
+  (testing "Return nil with wrong password"
+    (is (not (verify-password (str "a" valid-password) valid-email)))))
